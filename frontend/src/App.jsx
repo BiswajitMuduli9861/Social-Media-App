4b59ebb8f3d4257232b2181f753636ccc32bfd72
Biswajit Muduli <bmuduli126@gmail.com>
2026-01-18 23:19:39 +0530

    feat: implement login, signup, profile and feed-stories layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Feed from './pages/Feed'
+import Message from './pages/Message'
+import ChatBox from './pages/ChatBox'
+import Connection from './pages/Connection'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import { useUser } from '@clerk/clerk-react'
+import {Toaster} from 'react-hot-toast'
+function App() {
+
+  const {user} = useUser()
+  return (
+    <>
+    <Toaster/>
+      <Routes>
+
+        <Route path='/' element={!user ? <Login/> : <Layout/>}>
+        <Route index element={<Feed/>}/>
+        <Route path='messages' element={<Message/>}/>
+        <Route path='messages/:userId' element={<ChatBox/>}/>
+        <Route path='connections' element={<Connection/>}/>
+        <Route path='discover' element={<Discover/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='profile/:profileId' element={<Profile/>}/>
+        <Route path='create-post' element={<CreatePost/>}/>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Connection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Connection = () => {
+  return (
+    <div className='bg-amber-600'>
+      hiii
+    </div>
+  )
+}
+
+export default Connection
